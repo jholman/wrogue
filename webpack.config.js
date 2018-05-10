9c087c80b9d5e38af458946289cc8ae431d97b55
Jeremy Holman <jeremy.holman@gmail.com>
2018-05-09 21:24:05 -0700

    initial commit, copied from web-boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+
+  // https://webpack.js.org/configuration/output/
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js'
+  },
+
+  // https://webpack.js.org/configuration/devtool/#devtool
+  devtool: 'source-map',
+
+  // https://webpack.js.org/configuration/dev-server/
+  devServer: {
+    contentBase: path.resolve(__dirname, 'src/public'),
+    host: '0.0.0.0',
+    proxy: {
+      '/api': 'http://localhost:8081'
+    }
+  },
+
+  module: {
+    rules: [
+
+      // https://webpack.js.org/loaders/babel-loader/
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+
+      // https://webpack.js.org/loaders/sass-loader/
+      {
+        test: /\.(c|sa|sc)ss$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader'
+        }, {
+          loader: 'sass-loader'
+        }]
+      }
+    ]
+  }
+};
